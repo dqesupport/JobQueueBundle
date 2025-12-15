570cf24a70740e77c1b935834e1e676bef2867d2
skirchioglo <stepan.k@avantajprim.com>
2025-12-15 13:33:14 +0200

    id with comma

--- Admin/JobAdmin.php
+++ Admin/JobAdmin.php
@@ -108,7 +108,13 @@ class JobAdmin extends AbstractAdmin
                     'label' => 'admin.queue',
                 ]
             )
-            ->add('id', null, ['label' => 'admin.id'])
+            ->add(
+                'id',
+                'string',
+                [
+                    'label' => 'admin.id'
+                ]
+            )
             ->add('state', null, ['label' => 'admin.state'])
             ->add('queue', null, ['label' => 'admin.queue'])
             ->add('priority', null, ['label' => 'admin.priority'])
