079c9e6d4efb1b0cae6ff6a638a249fa11785655
Alexandr Babii <a.babi@avantajprim.com>
2026-05-28 11:29:20 +0300

    CMS-947: поддержка doctrine/orm 3

--- Command/RunCommand.php
+++ Command/RunCommand.php
@@ -4,7 +4,7 @@ namespace Octava\Bundle\JobQueueBundle\Command;
 
 use JMS\JobQueueBundle\Command\RunCommand as BaseRunCommand;
 use JMS\JobQueueBundle\Entity\Repository\JobManager;
-use Symfony\Bridge\Doctrine\ManagerRegistry;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
